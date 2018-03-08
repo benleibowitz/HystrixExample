dcc37cceaa4f2988b6a8c1f166fa0f9057917d7e
benleibowitz <benleibowitz2013@gmail.com>
2018-03-07 20:27:46 -0500

    Serializable

--- src/main/java/com/ben/Person.java
+++ src/main/java/com/ben/Person.java
@@ -1,16 +1,23 @@
 package com.ben;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-public class Person {
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1091853598015252017L;
+
     @Id
     private String id;
 
